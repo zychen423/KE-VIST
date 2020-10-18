808d2174fce3e3a226b029c1b36f0de29bfab034
ethan5437 <32521027+ethan5437@users.noreply.github.com>
2020-10-18 17:11:25 +0800

    Create download_big_data.sh

---
+++ src/stage3/term2story/data/download_big_data.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+wget https://doraemon.iis.sinica.edu.tw/KE_VIST_AAAI2020/img_pair2path.vg.json
+wget https://doraemon.iis.sinica.edu.tw/KE_VIST_AAAI2020/ROC_replace_coref_gender_mapped_frame_noun.json
+wget https://doraemon.iis.sinica.edu.tw/KE_VIST_AAAI2020/VIST_coref_nos_mapped_frame_noun_train.json
